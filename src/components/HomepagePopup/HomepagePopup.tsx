72b0733a1507a4370d51dfc45858f228660c4eed
ZoHoC <fajrunt.tw@gmail.com>
2023-04-05 21:48:14 +0200

    Visual style changes

--- src/components/HomepagePopup/HomepagePopup.tsx
+++ src/components/HomepagePopup/HomepagePopup.tsx
@@ -1,18 +1,24 @@
+import Button from "../Button/Button";
 import styles from "./HomepagePopup.module.scss";
 
 const HomepagePopup = () => {
-  console.log(styles);
-
   return (
     <div className={styles["HomepagePopup"]}>
       <div className={styles["HomepagePopup_overlay"]}>
         <div className={styles["HomepagePopup-Inner"]}>
-          <div className={styles["HomepagePopup-Container"]}>
+          <div className={` ${styles["HomepagePopup-Container"]} ${styles["HomepagePopup-Container_gap"]}`}>
             <p className={styles["HomepagePopup-Title"]}>Make MyNews your homepage</p>
             <p className={styles["HomepagePopup-Subtitle"]}>Every day discover what’s trending on the internet!</p>
           </div>
           <div className={styles["HomepagePopup-Container"]}>
-            <button>GET</button>
+            <Button
+              isSecondary
+              handleClick={function (): {} {
+                throw new Error("Function not implemented.");
+              }}
+            >
+              get
+            </Button>
             <p className={styles["HomepagePopup-Title"]}>No, thanks</p>
           </div>
         </div>
