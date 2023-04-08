2f6f463d5a3fb70eb386dc5fceb8740fe37da92e
ZoHoC <fajrunt.tw@gmail.com>
2023-04-08 17:58:23 +0200

    Added sass to devDep

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
