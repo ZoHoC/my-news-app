b98c754cf46a5f9023016b12e0df5fe15b4e67e1
ZoHoC <fajrunt.tw@gmail.com>
2023-04-11 00:29:37 +0200

    added dynamic news article rendering

--- src/modules/LatestNews/LatestNews.tsx
+++ src/modules/LatestNews/LatestNews.tsx
@@ -2,8 +2,11 @@ import styles from "./LatestNews.module.scss";
 import Link from "next/link";
 import ArrowRight from "../../../public/assets/icons/ArrowRight.svg";
 import LatestNewsArticle from "@/components/LatestNewsArticle/LatestNewsArticle";
+import { useAppSelector } from "@/utility/hooks";
 
 const LatestNews = () => {
+  const { newsData } = useAppSelector(state => state.news);
+
   return (
     <aside className={styles["LatestNews"]}>
       <div className={styles["LatestNews-Wrapper"]}>
@@ -13,17 +16,13 @@ const LatestNews = () => {
         <h2 className={styles["LatestNews-Title"]}>Latest news</h2>
       </div>
       <div className={styles["LatestNews-Board"]}>
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
-        <LatestNewsArticle />
+        {newsData.map(({ id, title, publishedDate }) => (
+          <LatestNewsArticle
+            key={id}
+            title={title}
+            publishedDate={publishedDate}
+          />
+        ))}
       </div>
       <div className={styles["LatestNews-Wrapper"]}>
         <Link href={"#"} className={styles["LatestNews-Link"]}>
