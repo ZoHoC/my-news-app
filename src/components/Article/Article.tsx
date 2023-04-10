7162aed908d055313b124a73a5c8430858fad718
ZoHoC <fajrunt.tw@gmail.com>
2023-04-11 00:27:52 +0200

    Removed placeholder items for props.

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,36 +1,49 @@
 import Link from "next/link";
 import styles from "./Article.module.scss";
 import Image from "next/image";
-import placeholder from "../../../public/assets/images/placeholder.png";
-import FavoriteIcon from "../../../public/assets/icons/FavoriteIcon.svg";
+import FavouriteIcon from "../../../public/assets/icons/FavouriteIcon.svg";
+import { FC } from "react";
 
-const Article = () => {
+const Article: FC<ArticleProps> = ({
+  id,
+  section,
+  title,
+  imageCaption,
+  imageUrl,
+  author,
+}) => {
   return (
     <article className={styles["Article"]}>
       <Link href={"#"}>
         <figure className={styles["Article-Figure"]}>
           <Image
-            src={placeholder}
-            alt="placeholder"
+            src={imageUrl}
+            alt={imageCaption}
             className={styles["Article-Image"]}
+            width={440}
+            height={293}
           />
         </figure>
         <div className={styles["Article-Wrapper"]}>
           <div className={styles["Article-Container"]}>
-            <p className={styles["Article-NewsSection"]}>Technology</p>
-            <FavoriteIcon />
+            <p className={styles["Article-NewsSection"]}>{section}</p>
+            <FavouriteIcon />
           </div>
-          <h2 className={styles["Article-Title"]}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
-            tenetur soluta corporis fuga, magni eum vitae laborum obcaecati eos
-            rerum ad beatae illo magnam quasi alias? Mollitia eveniet doloribus
-            quisquam.
-          </h2>
-          <p className={styles["Article-Author"]}>Isaac Salazar</p>
+          <h2 className={styles["Article-Title"]}>{title}</h2>
+          <p className={styles["Article-Author"]}>{author}</p>
         </div>
       </Link>
     </article>
   );
 };
+
+interface ArticleProps {
+  id?: number;
+  section: string;
+  title: string;
+  imageCaption: string;
+  imageUrl: string;
+  author: string;
+}
 
 export default Article;
