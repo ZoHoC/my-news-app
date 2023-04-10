a8a8f1bd099f263e1a50479fc464763712a0205b
ZoHoC <fajrunt.tw@gmail.com>
2023-04-11 00:28:23 +0200

    Visual changes and title added

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,19 @@
 import styles from "./Grid.module.scss";
 import { FC, ReactNode } from "react";
 
-const Grid: FC<GridProps> = ({ children }) => {
-  return <div className={styles["Grid"]}>{children}</div>;
+const Grid: FC<GridProps> = ({ title, children }) => {
+  return (
+    <div className={styles["Grid"]}>
+      <div className={styles["Grid-Header"]}>
+        {title && <h2 className={styles["Grid-Title"]}>{title}</h2>}
+      </div>
+      {children}
+    </div>
+  );
 };
 
 interface GridProps {
+  title?: string;
   children: ReactNode;
 }
 
