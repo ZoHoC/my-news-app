0b7703b98fbcdf81d0f6eefe31902419ba881121
ZoHoC <fajrunt.tw@gmail.com>
2023-04-11 17:33:38 +0200

    changed data

--- src/modules/NavBar/NavBar.tsx
+++ src/modules/NavBar/NavBar.tsx
@@ -1,32 +1,33 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import NavBarLink from "@/components/NavBarLink/NavBarLink";
 import styles from "./NavBar.module.scss";
-import { navData, navDataItem } from "@/utility/navData";
+import { useAppDispatch, useAppSelector } from "@/utility/hooks";
+import { NavDataItem, toggleChoice } from "@/redux/reducer/navDataReducer";
+import { iconMap } from "@/utility/iconMap";
 
 const NavBar: FC = () => {
-  const [data, setData] = useState<navDataItem[]>(navData);
+  const dispatch = useAppDispatch();
+  const { navData } = useAppSelector(state => state.navData);
+  const [data, setData] = useState<NavDataItem[]>([]);
+
+  useEffect(() => {
+    setData(navData.map(item => ({ ...item, icon: iconMap[item.title] })));
+  }, [navData]);
 
   const handleClick = (id: number) => {
-    setData(
-      data.map((data: navDataItem) => {
-        if (data.id === id) {
-          return { ...data, isPressed: true };
-        } else {
-          return { ...data, isPressed: false };
-        }
-      })
-    );
+    dispatch(toggleChoice(id));
   };
 
   return (
     <div className={styles["NavBar"]}>
       <div className={styles["NavBar-Grid"]}>
-        {data.map(({ icon, title, id, isPressed }) => (
+        {data.map(({ icon, title, id, link, isPressed }) => (
           <NavBarLink
             key={id}
             id={id}
             icon={icon}
             title={title}
+            link={link}
             isPressed={isPressed}
             handleClick={handleClick}
           />
