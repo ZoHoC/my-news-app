414295d8bad4dced217c5aa82c6a922b466fe342
ZoHoC <fajrunt.tw@gmail.com>
2023-04-13 17:50:45 +0200

    added infinite scroll

--- src/modules/LatestNews/LatestNews.tsx
+++ src/modules/LatestNews/LatestNews.tsx
@@ -2,10 +2,25 @@ import styles from "./LatestNews.module.scss";
 import Link from "next/link";
 import ArrowRight from "../../../public/assets/icons/ArrowRight.svg";
 import LatestNewsArticle from "@/components/LatestNewsArticle/LatestNewsArticle";
-import { useAppSelector } from "@/utility/hooks";
+import { useAppDispatch, useAppSelector } from "@/utility/hooks";
+import { fetchNewsData } from "@/redux/actions/fetchNewsDataAction";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const LatestNews = () => {
+  const [hasMore, setHasMore] = useState(true);
   const { newsData } = useAppSelector(state => state.news);
+  const dispatch = useAppDispatch();
+
+  const fetchMoreData = () => {
+    dispatch(
+      fetchNewsData({ section: "all", limit: 30, offset: newsData.length })
+    ).then(action => {
+      if (action.payload.length === 0) {
+        setHasMore(false);
+      }
+    });
+  };
 
   return (
     <aside className={styles["LatestNews"]}>
@@ -15,15 +30,23 @@ const LatestNews = () => {
         </div>
         <h2 className={styles["LatestNews-Title"]}>Latest news</h2>
       </div>
-
-      <div className={styles["LatestNews-Board"]}>
-        {newsData.map(({ title, publishedDate }, index) => (
-          <LatestNewsArticle
-            key={index}
-            title={title}
-            publishedDate={publishedDate}
-          />
-        ))}
+      <div id="scrollableDiv" className={styles["LatestNews-Board"]}>
+        <InfiniteScroll
+          dataLength={newsData.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          scrollableTarget="scrollableDiv"
+          loader={<h4>Loading...</h4>}
+          endMessage={<p>No more news to show.</p>}
+        >
+          {newsData.map(({ title, publishedDate }, index) => (
+            <LatestNewsArticle
+              key={index}
+              title={title}
+              publishedDate={publishedDate}
+            />
+          ))}
+        </InfiniteScroll>
       </div>
       <div className={styles["LatestNews-Wrapper"]}>
         <Link href={"#"} className={styles["LatestNews-Link"]}>
